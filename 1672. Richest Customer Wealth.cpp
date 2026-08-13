98139c88a18c6ccc6c65565fc872e299e218fb83
Raunit Bakshi <raunitbakshi@gmail.com>
2026-08-13 10:18:06 +0530

    Update 1672. Richest Customer Wealth.cpp

--- 1672. Richest Customer Wealth.cpp
+++ 1672. Richest Customer Wealth.cpp
@@ -1,1 +1,15 @@
-public 
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int current_sum , maxsum = 0;
+        for (int i = 0; i<accounts.size(); i++){
+            current_sum = 0;
+            for (int j = 0; j<accounts[0].size(); j++){
+                current_sum += accounts[i][j];
+            }
+            maxsum = max (maxsum , current_sum);
+        }
+    
+        return maxsum;
+        }
+};

fcc9f1067cffe894736375f679009ef911af7e31
Raunit Bakshi <raunitbakshi@gmail.com>
2026-08-17 00:49:57 +0530

    Implement matrix reshape function in Solution class

--- 566.cpp
+++ 566.cpp
@@ -2,5 +2,27 @@ class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         
+        if (mat.size() * mat[0].size() != r * c) {
+            return mat;
+        }
+
+        int oldCols = mat[0].size();
+
+        vector<vector<int>> ans(r, vector<int>(c));
+
+        int k = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+
+                int oldRow = k / oldCols;
+                int oldCol = k % oldCols;
+
+                ans[i][j] = mat[oldRow][oldCol];
+
+                k++;
+            }
+        }
+        return ans;
     }
 };

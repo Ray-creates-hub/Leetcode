9d28424a6fd8daed0e0f33c0f2ea4e2929a0f65a
Raunit Bakshi <raunitbakshi@gmail.com>
2026-08-20 12:35:25 +0530

    Implement missing number finder in Solution class

--- 268. Missing Number.cpp
+++ 268. Missing Number.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
 
+        vector<int> freq(nums.size() + 1, 0);
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (int i = 0; i < freq.size(); i++) {
+
+            if (freq[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};

5f404d4923f4a71fb492fe609427ec0b54eeed2f
Raunit Bakshi <raunitbakshi@gmail.com>
2026-08-21 16:03:47 +0530

    Implement pivot index calculation in Solution class
    Refactor pivotIndex method to calculate left and right sums for each index.

--- 724.cpp
+++ 724.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
+
+        int right_sum, left_sum;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            right_sum = 0;
+            left_sum = 0;
+
+            for (int j = 0; j < nums.size(); j++) {
+
+                if (j == i) {
+                    continue;
+                }
+
+                if (j < i) {
+                    left_sum += nums[j];
+                }
+                else {
+                    right_sum += nums[j];
+                }
+            }
+
+            if (right_sum == left_sum) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 };

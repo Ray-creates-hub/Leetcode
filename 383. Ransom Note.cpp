41ce636aedc9bbde05a197b8993efdecbaaca74d
Raunit Bakshi <raunitbakshi@gmail.com>
2026-08-23 00:54:55 +0530

    Implement canConstruct function for ransom note

---
+++ 383. Ransom Note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        vector <int> freq(26,0);
+
+        for ( char ch : magazine ){
+            freq[ch-'a']++;
+        }
+        for ( char ch : ransomNote ){
+            freq[ch-'a']--;
+        }
+
+        for ( int count : freq ){
+            if(count<0){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+};

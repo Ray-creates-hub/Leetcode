0f6e92f326add1278f9b6e3cae5d577c024f3e37
Raunit Bakshi <raunitbakshi@gmail.com>
2026-09-05 17:34:51 +0530

    Implement maxArea function for water container problem

---
+++ 11. Container With Most Water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int Area = min(height[left], height[right]) * (right - left);
+            maxArea = max(maxArea, Area);
+            if (height[left] < height[right]) {
+                left++;
+            }
+            else right--;
+        }
+        return maxArea;
+    }
+};
